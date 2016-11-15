bff8b2884da5eba04de4cc635ba3fae7ce10c225
afghifari <alhudaghifari@gmail.com>
2016-11-15 23:25:42 +0700

    NB baru tahap awal

---
+++ src/NB_buatan_sendiri/AttributeNominal.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes2ai;
+
+/**
+ *
+ * @author Ghifari
+ */
+public class AttributeNominal {
+    private Object atribut[][];
+    private int countAtribut[][];
+    
+    public AttributeNominal() {
+    
+    }
+    
+    public AttributeNominal(int nDataDifferent, int nConclusion) {
+        atribut = new Object[nDataDifferent][nConclusion];
+        countAtribut = new int[nDataDifferent][nConclusion];
+    }
+    
+    public Object[] getAttribArrayObjectType(){
+        return atribut;
+    }
+    
+    public void setAtribut(int nDataDifferent, int nConclusion, Object data){
+        atribut[nDataDifferent][nConclusion] = data;
+    }
+    
+    public Object getAttribObjectType(int i){
+        return atribut[i];
+    }
+    
+    public void setCountAtribut(int nDataDifferent, int nConclusion, int value) {
+        countAtribut[nDataDifferent][nConclusion] = value;
+    }
+    
+    public int getCountAtribut(int index, int nConclusion) {
+        return countAtribut[index][nConclusion];
+    }
+}
